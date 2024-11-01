824f6fe9c243da8b5eef8767d1ea000a099eba6c
Richard Clacksman <rc@wordwall.co.uk>
2024-11-01 17:44:55 +0000

    Got camera working pretty well

--- AntDefense/Assets/AntCam.cs
+++ AntDefense/Assets/AntCam.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class AntCam : MonoBehaviour
 {
-    public float CameraMoveSpeed = 100;
+    private const int MouseButton = 1;
+    public float CameraZoomSpeed = 150;
+    public float CameraPanSpeed = 0.8f;
+    public float CameraSidewaysSpeed = 0.5f;
+    private Vector3 lastMousePosition;
 
     // Start is called before the first frame update
     void Start()
@@ -12,14 +16,35 @@ public class AntCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float newX = transform.position.x;
+        float newY = transform.position.y;
+        float newZ = transform.position.z;
         if (Input.mouseScrollDelta.y != 0)
         {
-            float newY = transform.position.y - Input.mouseScrollDelta.y * Time.deltaTime * CameraMoveSpeed;
-            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            newY -= Input.mouseScrollDelta.y * Time.deltaTime * CameraZoomSpeed;
 
-            var newRotationX = transform.rotation.x - Input.mouseScrollDelta.y * Time.deltaTime * 0.5f;
+            var newRotationX = transform.rotation.x - Input.mouseScrollDelta.y * Time.deltaTime * CameraPanSpeed;
             transform.rotation = new Quaternion((float)newRotationX, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-            Debug.Log($"Rotation {transform.rotation}");
+            //Debug.Log($"Rotation {transform.rotation}");
         }
+
+        if (Input.GetMouseButtonDown(MouseButton))
+        {
+            lastMousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(MouseButton))
+        {
+            var change = Input.mousePosition - lastMousePosition;
+            lastMousePosition = Input.mousePosition;
+            if(change.magnitude > 0)
+            {
+                //Debug.Log("Mouse Move " + change);
+                //transform.position += change;
+                newX -= change.x * CameraSidewaysSpeed;
+                newZ -= change.y * CameraSidewaysSpeed;
+            }
+        }
+        transform.position = new Vector3(newX, newY, newZ);
     }
+
 }
