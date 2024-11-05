3575cbe851bb3eab57333d11d031dedb88f0874d
Richard Clacksman <rc@wordwall.co.uk>
2024-11-05 13:04:46 +0000

    Removed the old position provider

--- AntDefense/Assets/AntTargetPositionProvider.cs
+++ AntDefense/Assets/AntTargetPositionProvider.cs
@@ -1,44 +1,87 @@
-using UnityEngine;
+﻿using UnityEngine;
 
+//TODO - Make turn around work by setting the heading direction rather than just generally turning it around.
+//the direction can then be allowed to wander back normal with its random movements.
+// For obstacles the target position should be set to a position along the tangent of teh collision.
+// For turning around to find the way back it should be set behind the ant.
+// When the ant has a target it's moving towards,don't just use that location imediately, instead have the target wander back towards it.
 public class AntTargetPositionProvider : MonoBehaviour, ITargetPositionProvider
 {
-    public float ForwardsBias = 0.1f;
-    public float RandomBias = 0.2f;
+    /// <summary>
+    /// Rate at which the position the ant is currently turning towards moves towards the target.
+    /// If there is no currnet target, the target is taken as being straight ahead.
+    /// </summary>
+    public float ForwardsBias =1f;
 
     /// <summary>
-    /// Target position relative to this ant
+    /// Multiplier for <see cref="ForwardsBias"/> applied when there is a target, to get the ant to turn towards the target faster.
     /// </summary>
-    private Vector3 _targetDirection = Vector3.forward;
+    public float TargetMultiplier = 1.2f;
+
+    /// <summary>
+    /// Rate at which teh ant's turning direction moves randomly.
+    /// </summary>
+    public float RandomBias = 1f;
+
+    /// <summary>
+    /// World space target direction
+    /// </summary>
+    public Vector3 TargetDirection {  get; private set; }
 
     /// <summary>
     /// Target position in world space
     /// </summary>
-    public Vector3 TargetPosition => TargetObject?.position ?? transform.position + transform.TransformDirection(_targetDirection);
+    public Vector3 TargetPosition => transform.position + TargetDirection;
 
-    public Transform TargetObject => AntStateMachine.CurrentTarget?.TargetPoint;
+    /// <summary>
+    /// The non-randomised target position that this ant should be turning towards.
+    /// In world space
+    /// </summary>
+    private Vector3 _eventualTargetDirection = Vector3.zero;
 
     public TurnAround? TurnAround => AntStateMachine.TurnAroundState;
 
-    public AntStateMachine AntStateMachine;
+    private AntStateMachine AntStateMachine;
+
+    void Start()
+    {
+        AntStateMachine = GetComponentInChildren<AntStateMachine>();
+        _eventualTargetDirection = transform.forward; // default to walking forwards.
+        var randomPosition = Random.insideUnitCircle.normalized;
+        TargetDirection = new Vector3(randomPosition.x, 0, randomPosition.y);    // Start with the current target position in a random direction.
+    }
 
     void FixedUpdate()
     {
-        if(TargetObject == null)
+        var targetObject = AntStateMachine.CurrentTarget?.TargetPoint;
+        var forwardsBias = ForwardsBias * Time.fixedDeltaTime;
+
+        if (targetObject != null)
         {
-            _targetDirection += Random.insideUnitSphere * RandomBias + Vector3.forward * ForwardsBias;
-            _targetDirection.y *= 0.1f;
-            _targetDirection.Normalize();
+            forwardsBias *= TargetMultiplier;
+            _eventualTargetDirection = targetObject.position - transform.position;
+            this.TargetDirection = _eventualTargetDirection;
+            // TODO don't set it to the target immediately after a recent collision, let it move back gradually for some time,
+            // only return to jumping straight to the target after some time without a collision.
+            return;
         }
         else
         {
-            _targetDirection = Vector3.zero;
+            _eventualTargetDirection = transform.forward;
         }
 
-        Debug.DrawRay(transform.position, transform.TransformDirection(_targetDirection), Color.gray);
-        Debug.DrawLine(transform.position, TargetPosition, Color.black);
+        var randomBias = RandomBias * Time.fixedDeltaTime;
+        var randomComponent = (Random.insideUnitSphere - TargetDirection) * randomBias;
+        var forwardsComponent = (_eventualTargetDirection - TargetDirection) * forwardsBias;
+        Debug.DrawRay(transform.position, TargetDirection, Color.red);
+        Debug.DrawRay(TargetPosition, forwardsComponent, Color.white);
+        Debug.DrawRay(TargetPosition + forwardsComponent, randomComponent, Color.gray);
+        this.TargetDirection += forwardsComponent + randomComponent;
+
+        Debug.DrawRay(transform.position, _eventualTargetDirection, Color.black);
+        Debug.DrawRay(transform.position, TargetDirection, Color.green);
     }
 }
-
 public interface ITargetPositionProvider
 {
     Vector3 TargetPosition { get; }
@@ -58,8 +101,8 @@ public readonly struct TurnAround
         Clockwise = clockwise;
     }
 
-    public static TurnAround AvoidObstacle(bool clockwise) => new TurnAround (TurnAroundMode.AvoidObstacle, clockwise);
-    public static TurnAround LookAround(bool clockwise) => new TurnAround (TurnAroundMode.LookAround, clockwise);
+    public static TurnAround AvoidObstacle(bool clockwise) => new TurnAround(TurnAroundMode.AvoidObstacle, clockwise);
+    public static TurnAround LookAround(bool clockwise) => new TurnAround(TurnAroundMode.LookAround, clockwise);
 }
 
 public enum TurnAroundMode
