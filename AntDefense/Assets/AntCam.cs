e400b4b3a4f60aebe2d1810ca5f30316511fe566
Richard Clacksman <rc@wordwall.co.uk>
2024-12-03 20:17:14 +0000

    WIP camera improvements

--- AntDefense/Assets/AntCam.cs
+++ AntDefense/Assets/AntCam.cs
@@ -2,31 +2,26 @@ using UnityEngine;
 
 public class AntCam : MonoBehaviour
 {
-    private const int MouseButton = 1;
+    private const int MouseButton = 2;
     public float CameraZoomSpeed = 150;
     public float CameraPanSpeed = 0.8f;
     public float CameraSidewaysSpeed = 0.5f;
     private Vector3 lastMousePosition;
 
-    // Start is called before the first frame update
-    void Start()
+    public Camera Camera;
+
+    private void Start()
     {
+        _targetXRotation = transform.rotation.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         float newX = transform.position.x;
-        float newY = transform.position.y;
         float newZ = transform.position.z;
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            newY -= Input.mouseScrollDelta.y * Time.deltaTime * CameraZoomSpeed;
 
-            var newRotationX = transform.rotation.x - Input.mouseScrollDelta.y * Time.deltaTime * CameraPanSpeed;
-            transform.rotation = new Quaternion((float)newRotationX, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-            //Debug.Log($"Rotation {transform.rotation}");
-        }
+        HandleZoom();
 
         if (Input.GetMouseButtonDown(MouseButton))
         {
@@ -36,7 +31,7 @@ public class AntCam : MonoBehaviour
         {
             var change = Input.mousePosition - lastMousePosition;
             lastMousePosition = Input.mousePosition;
-            if(change.magnitude > 0)
+            if (change.magnitude > 0)
             {
                 //Debug.Log("Mouse Move " + change);
                 //transform.position += change;
@@ -44,7 +39,34 @@ public class AntCam : MonoBehaviour
                 newZ -= change.y * CameraSidewaysSpeed;
             }
         }
-        transform.position = new Vector3(newX, newY, newZ);
+        transform.position = new Vector3(newX, transform.position.y, newZ);
     }
 
+    private float _targetXRotation = 0;
+    public float MinY = 10;
+
+    private void HandleZoom()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            float currentY = transform.InverseTransformPoint(Camera.transform.position).y;
+            var newY = currentY - Input.mouseScrollDelta.y * Time.deltaTime * CameraZoomSpeed;
+            
+            var actualNewY = Mathf.Max(currentY, MinY);
+            if(currentY <= MinY && newY <= MinY)
+            {
+                // already as low as it can go.
+                return;
+            }
+
+            _targetXRotation = _targetXRotation - Input.mouseScrollDelta.y * Time.deltaTime * CameraPanSpeed;
+            var newRotationX = Mathf.Min(0, _targetXRotation);
+            //Debug.Log($"Target: {_targetXRotation}, actual: {newRotationX}");
+            transform.rotation = new Quaternion((float)newRotationX, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            //Debug.Log($"Rotation {transform.rotation}");
+
+            Camera.transform.position = transform.TransformPoint(new Vector3(0, actualNewY, 0));
+        }
+
+    }
 }
