5db0f7a59d8e6c254841cddcb8a8836286b743a1
Richard Clacksman <rc@wordwall.co.uk>
2024-12-04 21:46:22 +0000

    Major improvements to the camera

--- AntDefense/Assets/AntCam.cs
+++ AntDefense/Assets/AntCam.cs
@@ -5,7 +5,8 @@ public class AntCam : MonoBehaviour
     private const int MouseButton = 2;
     public float CameraZoomSpeed = 150;
     public float CameraPanSpeed = 0.8f;
-    public float CameraSidewaysSpeed = 0.5f;
+    public float MinCameraSpeed = 0.1f;
+    public float MaxCameraSpeed = 0.5f;
     private Vector3 lastMousePosition;
 
     public Camera Camera;
@@ -29,14 +30,16 @@ public class AntCam : MonoBehaviour
         }
         if (Input.GetMouseButton(MouseButton))
         {
+            var zoomProportion = GetZoomProportion(CurrentY);
+            var speed = GetProportionOfRange(zoomProportion, MinCameraSpeed, MaxCameraSpeed);
             var change = Input.mousePosition - lastMousePosition;
             lastMousePosition = Input.mousePosition;
             if (change.magnitude > 0)
             {
                 //Debug.Log("Mouse Move " + change);
                 //transform.position += change;
-                newX -= change.x * CameraSidewaysSpeed;
-                newZ -= change.y * CameraSidewaysSpeed;
+                newX -= change.x * speed;
+                newZ -= change.y * speed;
             }
         }
         transform.position = new Vector3(newX, transform.position.y, newZ);
@@ -49,36 +52,42 @@ public class AntCam : MonoBehaviour
     public float MaxAngleHeight = 100;
     public float MaxAngle = 0;
 
+    private float CurrentY => transform.InverseTransformPoint(Camera.transform.position).y;
+    private float GetZoomProportion(float actualNewY)
+    {
+        return Mathf.Clamp01((actualNewY - MinY) / (MaxAngleHeight - MinY));
+    }
+
     private void HandleZoom()
     {
         if (Input.mouseScrollDelta.y != 0)
         {
-            float currentY = transform.InverseTransformPoint(Camera.transform.position).y;
+            // TODO consider make this work on the proportion first, and get both angle and distance from that.
+            float currentY = CurrentY;
             var speedMultiplyer = currentY / 100;
             var newY = currentY - Input.mouseScrollDelta.y * CameraZoomSpeed * speedMultiplyer;
-            
+
             var actualNewY = Mathf.Clamp(newY, MinY, MaxY);
-            if(currentY <= MinY && newY <= MinY)
+            if (currentY == actualNewY)
             {
-                // already as low as it can go.
                 return;
             }
 
-            var newYProportion = (newY - MinY) / (MaxAngleHeight - MinY);
+            var zoomProportion = GetZoomProportion(actualNewY);
 
-            // TODO check the maths for this so that it sets the angle to the same proportion through its range as the height is.
-            var newAngle = MinAngle + (MaxAngle - MinAngle) * newYProportion;
+            var newAngle = GetProportionOfRange(zoomProportion, MinAngle, MaxAngle);
 
             _targetXRotation = _targetXRotation - Input.mouseScrollDelta.y * CameraPanSpeed * speedMultiplyer;
-            var newRotationX = Mathf.Clamp(_targetXRotation, MinAngle, MaxAngle);
 
-            //newRotationX = newAngle;
-            //Debug.Log($"Target: {_targetXRotation}, actual: {newRotationX}");
-            transform.rotation = new Quaternion((float)newRotationX, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-            //Debug.Log($"Rotation {transform.rotation}");
+            transform.rotation = Quaternion.AngleAxis(newAngle, Vector3.right);
 
             Camera.transform.position = transform.TransformPoint(new Vector3(0, actualNewY, 0));
         }
 
+    }
+
+    private static float GetProportionOfRange(float proportion, float min, float max)
+    {
+        return min + ((max - min) * proportion);
     }
 }
