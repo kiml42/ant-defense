dbc6ddf10dbec6a053e8d68d6724dc11d6396f7f
Richard Clacksman <rc@wordwall.co.uk>
2024-12-16 20:26:19 +0000

    Added ability to spawn objects

---
+++ AntDefense/Assets/ObjectPlacer.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public List<GameObject> QuickBarObjects;
+
+    private KeyCode[] _quickBarKeys = {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3,
+        KeyCode.Alpha4,
+        KeyCode.Alpha5,
+        KeyCode.Alpha6,
+        KeyCode.Alpha7,
+        KeyCode.Alpha8,
+        KeyCode.Alpha9,
+        KeyCode.Alpha0,
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _quickBarKeys.Length; i++)
+        {
+            if (Input.GetKeyUp(_quickBarKeys[i]))
+            {
+                SpawnQuickObject(i);
+            }
+        }
+    }
+
+    private void SpawnQuickObject(int i)
+    {
+        if (QuickBarObjects.Count <= i) return;
+        var prefab = QuickBarObjects[i];
+        var newObject = Instantiate(prefab, this.transform.position, Quaternion.identity);
+    }
+}
