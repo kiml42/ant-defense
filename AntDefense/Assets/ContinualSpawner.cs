917d726376e7e397ed6b2b19bd12f451d37d6cf7
Richard Clacksman <kiml42a@gmail.com>
2025-08-05 15:50:09 +0100

    Added more generic continual spawner class

---
+++ AntDefense/Assets/ContinualSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ContinualSpawner : MonoBehaviour
+{
+    public GameObject ParentForSpawnedObjects;
+    public Transform PrefabToSpawn;
+    public Transform DefaultSpawnPoint;
+
+    public Vector3 SpawnPositionRandomisation;
+
+    public float CountPerSpawn = 1;
+
+    public float MinRespawnTime = 1;
+    public float MaxRespawnTime = 5;
+
+    private float _timeUntilSpawn = 0;
+
+    void Start()
+    {
+        if (ParentForSpawnedObjects == null)
+        {
+            ParentForSpawnedObjects = this.gameObject;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _timeUntilSpawn -= Time.fixedDeltaTime;
+        if (_timeUntilSpawn < 0)
+        {
+            for (int i = 0; i < CountPerSpawn; i++)
+            {
+                var randomisation = Random.insideUnitSphere;
+                randomisation.Scale(SpawnPositionRandomisation);
+
+                var position = (DefaultSpawnPoint?.position ?? this.transform.position) + new Vector3(randomisation.x, 0, randomisation.y);
+                Instantiate(PrefabToSpawn, position, Quaternion.identity, ParentForSpawnedObjects.transform);
+            }
+            _timeUntilSpawn = Random.Range(MinRespawnTime, MaxRespawnTime);
+        }
+    }
+}
