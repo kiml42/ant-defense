1fcf163748ea4903330d7a0b21f38a5b6796161b
Richard Clacksman <kiml42a@gmail.com>
2025-11-01 16:12:34 +0000

    Added time controls

---
+++ AntDefense/Assets/Scripts/UI/GlobalKeyHandler.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class GlobalKeyHandler : MonoBehaviour
+{
+    // TODO: camera doesn't move when paused.
+    // TODO: Bug: walls still look see-through when paused.
+    public KeyCode TogglePauseKey = KeyCode.Space;
+
+    public KeyCode FastForwardModeKey = KeyCode.F;
+
+    public float FastForwardTimeScale = 3f;
+    public enum TimeScaleMode
+    {
+        Paused,
+        Normal,
+        FastForward
+    }
+
+    private TimeScaleMode _currentMode = TimeScaleMode.Normal;
+
+    private float CurrentTimeScale
+    {
+        get
+        {
+            switch(this._currentMode)
+            {
+                case TimeScaleMode.Paused:
+                    return 0f;
+                case TimeScaleMode.Normal:
+                    return 1f;
+                case TimeScaleMode.FastForward:
+                    return this.FastForwardTimeScale;
+                default:
+                    return 1f;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var anyChange = false;
+        if (Input.GetKeyUp(this.TogglePauseKey))
+        {
+            if (this._currentMode == TimeScaleMode.Paused)
+            {
+                this._currentMode = TimeScaleMode.Normal;
+            }
+            else
+            {
+                this._currentMode = TimeScaleMode.Paused;
+            }
+            anyChange = true;
+        }
+        if (Input.GetKeyUp(this.FastForwardModeKey))
+        {
+            switch(this._currentMode)
+            {
+                case TimeScaleMode.Paused:
+                case TimeScaleMode.Normal:
+                    this._currentMode = TimeScaleMode.FastForward;
+                    break;
+                case TimeScaleMode.FastForward:
+                    this._currentMode = TimeScaleMode.Normal;
+                    break;
+            }
+            anyChange = true;
+        }
+
+        if (anyChange)
+        {
+            Time.timeScale = (int)this.CurrentTimeScale;
+            AudioListener.pause = this._currentMode == TimeScaleMode.Paused;
+        }
+    }
+}
