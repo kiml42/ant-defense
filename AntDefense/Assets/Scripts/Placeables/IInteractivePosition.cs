ca5eb258c26bb1ab6e56a7a4d9141621e43125e0
Richard Clacksman <kiml42a@gmail.com>
2025-11-17 20:19:36 +0000

    moved to own file

--- AntDefense/Assets/Scripts/Placeables/IInteractivePosition.cs
+++ AntDefense/Assets/Scripts/Placeables/IInteractivePosition.cs
@@ -1,11 +1,4 @@
-using UnityEngine;
-
 public interface IInteractivePosition : IKnowsPosition
 {
     void Interact();
-}
-
-public interface  IKnowsPosition
-{
-    Vector3 Position { get; }
 }
---
+++ AntDefense/Assets/Scripts/Placeables/IKnowsPosition.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface  IKnowsPosition
+{
+    Vector3 Position { get; }
+}
