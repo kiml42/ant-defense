ea14fb227949598da20c1d8e5dbd8512cd9db3cf
Richard Clacksman <kiml42a@gmail.com>
2025-12-27 21:30:51 +0000

    Added score generator

---
+++ AntDefense/Assets/Scripts/Placeables/ScoreGenerator.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ScoreGenerator : MonoBehaviour
+{
+    public int ScoreIncrement = 10;
+    public float IncrementTime = 10;
+    public int NumberOfOffsets = 10;
+
+    private static int index;
+
+    private float currentDelay;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.currentDelay = (this.IncrementTime / this.NumberOfOffsets) * index;
+        Debug.Log($"ScoreGenerator starting with delay of {this.currentDelay} seconds.");
+        index++;
+        index = index % this.NumberOfOffsets;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.currentDelay -= Time.fixedDeltaTime;
+        if (this.currentDelay <= 0)
+        {
+            Debug.Log($"ScoreGenerator adding {this.ScoreIncrement} points.");
+            ScoreTracker.AddScore(this.ScoreIncrement);
+            this.currentDelay = this.IncrementTime;
+        }
+    }
+}
+
+public static class ScoreTracker
+{
+    public static int Score { get; private set; } = 0;
+    public static void AddScore(int score)
+    {
+        Score += score;
+    }
+}
