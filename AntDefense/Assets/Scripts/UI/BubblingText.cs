134e10e306a0e0ce10bd08dbcdc8d1f57ca16ab9
Richard Clacksman <kiml42a@gmail.com>
2025-12-27 22:46:59 +0000

    Made text fade

--- AntDefense/Assets/Scripts/UI/BubblingText.cs
+++ AntDefense/Assets/Scripts/UI/BubblingText.cs
@@ -6,6 +6,7 @@ public class BubblingText : MonoBehaviour
     public TextMeshPro Text;
     public Vector3 Velocity;
     public float Lifetime = 1.0f;
+    public float FadeDuration = 0.5f;
 
     public void Initialise(string text, Color? colour = null)
     {
@@ -24,6 +25,9 @@ public class BubblingText : MonoBehaviour
     {
         this.transform.position += this.Velocity * Time.deltaTime;
         this.Lifetime -= Time.deltaTime;
+
+        this.Text.alpha = Mathf.Clamp01(this.Lifetime / this.FadeDuration);
+
         if (this.Lifetime <= 0)
         {
             Destroy(this.gameObject);
