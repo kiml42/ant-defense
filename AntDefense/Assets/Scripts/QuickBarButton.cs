588d23e4596a58ddd939640cdd048b309a4ffdcf
Richard Clacksman <31065594+kiml42@users.noreply.github.com>
2026-01-20 16:59:57 +0000

    Added mouseover effect to quick bar icons

--- AntDefense/Assets/Scripts/QuickBarButton.cs
+++ AntDefense/Assets/Scripts/QuickBarButton.cs
@@ -1,8 +1,44 @@
 ﻿using TMPro;
+using UnityEngine;
 
 public class QuickBarButton : ClickableButton
 {
     public PlaceableObjectOrGhost Ghost { get; set; }
 
     public TextMeshPro CostText;
+
+    public float MouseoverScale = 1.1f;
+    public float ScaleLerpSpeed = 10f;
+
+    private Vector3 _originalScale;
+    private Vector3 _targetScale;
+    private bool _isMouseover = false;
+
+    private void Start()
+    {
+        _originalScale = this.transform.localScale;
+        _targetScale = _originalScale;
+    }
+
+    private void Update()
+    {
+        // Smoothly lerp towards target scale
+        this.transform.localScale = Vector3.Lerp(
+            this.transform.localScale,
+            _targetScale,
+            Time.deltaTime * this.ScaleLerpSpeed
+        );
+    }
+
+    private void OnMouseEnter()
+    {
+        _isMouseover = true;
+        _targetScale = _originalScale * this.MouseoverScale;
+    }
+
+    private void OnMouseExit()
+    {
+        _isMouseover = false;
+        _targetScale = _originalScale;
+    }
 }
