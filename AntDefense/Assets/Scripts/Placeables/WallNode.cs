418b69fa366ece53444209988c24449968ac5e18
Richard Clacksman <kiml42a@gmail.com>
2026-03-22 14:43:33 +0000

    Update WallNode.cs

--- AntDefense/Assets/Scripts/Placeables/WallNode.cs
+++ AntDefense/Assets/Scripts/Placeables/WallNode.cs
@@ -111,6 +111,13 @@ public class WallNode : PlaceableSelectableGhostableMonoBehaviour, IPlaceablePos
             return;
         }
 
+        if (ObjectPlacer.Instance.WallNodeBeingPlaced != null
+            && ObjectPlacer.Instance.WallNodeBeingPlaced.ConnectedNode == null)  // placing a disconnected wall node
+        {
+            ObjectPlacer.Instance.StartPlacingWallConnectedTo(this);
+            return;
+        }
+
         if (
             ObjectPlacer.Instance.WallNodeBeingPlaced != null                   // A wall node is being placed
             && ObjectPlacer.Instance.WallNodeBeingPlaced.ConnectedNode != null  // It is connected to something
@@ -124,6 +131,7 @@ public class WallNode : PlaceableSelectableGhostableMonoBehaviour, IPlaceablePos
             if (placedObject != null)   // may not be able to place the object (e.g. too expensive), so do nothing.
             {
                 placedObject.GetComponent<WallNode>().RemoveNode();
+                ObjectPlacer.Instance.StartPlacingWallConnectedTo(this);
             }
         }
     }
